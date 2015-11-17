68ebec2798addd27358bf81e73629de1d70b3af9
fiercescott <colliers136@hotmail.com>
2015-11-17 13:14:12 -0800

    Create towerInterface.h

---
+++ towerInterface.h
@@ -0,0 +1,13 @@
+#ifndef TOWER_HEADER
+#define TOWER_HEADER
+typedef struct tower{
+    int type;
+    int x;
+    int y;
+    int fireRate;
+    int counter;
+    int damage;
+    int range;
+} tower;
+
+#endif
